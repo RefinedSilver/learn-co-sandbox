2b7200ae1f9daacb7720fa20633e8955bd3f38e4
Ian McKenzie <imckenzie777@gmail.com>
2020-04-24 05:25:06 +0000

    Automatically backed up by Learn

--- learning_comments.rb
+++ learning_comments.rb
@@ -6,5 +6,8 @@ puts (1 #+ 1)
 # Assign constant 3 to bare-word variable triangle_sides
 `trangle_sides`
 
-# From the Three Dog Night song:
-"Joy to the Wold (Jeremiah was a Bullfrog)"
+# From the Three Dog Night song: "Joy to the Wold (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
