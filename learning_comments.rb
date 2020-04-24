cc57043ee5a50a1603eaf6f9b2bfaeac4d3f8918
Ian McKenzie <imckenzie777@gmail.com>
2020-04-24 05:33:01 +0000

    Done.

---
+++ learning_comments.rb
@@ -0,0 +1,18 @@
+# Don't do this:
+# puts (1 #+ 1)
+
+# Perform a constant expression evaluation
+3
+# Assign constant 3 to bare-word variable triangle_sides
+#`trangle_sides`
+
+# From the Three Dog Night song: "Joy to the Wold (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+name = "Luca"
+ 
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in
+the store."
